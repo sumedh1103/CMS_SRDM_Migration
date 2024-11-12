05fd7c3f7bf831f96b083bfbc36000821c706c87
Shabana Khan <shabananiyamatkhan@gmail.com>
2024-11-12 16:17:20 +0530

    some changes

--- UI/src/app/app.module.ts
+++ UI/src/app/app.module.ts
@@ -1,18 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
-import { RouterModule } from '@angular/router';
-import { routes } from './app.routes'; // Import routes if needed
 
 @NgModule({
   declarations: [
-    AppComponent // Add your main component here
+    AppComponent // Declare AppComponent here
   ],
   imports: [
-    BrowserModule,
-    RouterModule.forRoot(routes) // Configure routes if `app.routes.ts` exports routes
+    BrowserModule
   ],
-  providers: [], // Import `app.config.ts` if you have services or configurations
-  bootstrap: [AppComponent] // The root component to bootstrap
+  bootstrap: [AppComponent] // Bootstrap the main AppComponent
 })
 export class AppModule { }
