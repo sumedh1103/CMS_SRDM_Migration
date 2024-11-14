a1b7bd0fc5356aa73a2ee1a7a6e35416d41f25b2
saurabhrapanwar <saurabhrapanwar@gmail.com>
2024-11-14 11:36:08 +0530

    resolving errors

--- UI/src/app/app.module.ts
+++ UI/src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
+import { RouterModule } from '@angular/router';
+import { routes } from './app.routes'; // Import routes if needed
 
 @NgModule({
   declarations: [
-    AppComponent // Declare AppComponent here
+    AppComponent // Add your main component here
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes) // Configure routes if `app.routes.ts` exports routes
   ],
-  bootstrap: [AppComponent] // Bootstrap the main AppComponent
+  providers: [], // Import `app.config.ts` if you have services or configurations
+  bootstrap: [AppComponent] // The root component to bootstrap
 })
 export class AppModule { }
